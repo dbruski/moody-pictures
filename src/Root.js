f6fd1efab50891d5c4275bf5658ab2fafd2ca9dc
dbruski <dawidbruski1@gmail.com>
2020-08-23 17:49:53 +0200

    Added initial state

--- src/Root.js
+++ src/Root.js
@@ -3,13 +3,14 @@ import { Provider } from './context';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { routes } from './routes';
 import MainTemplate from './templates/MainTemplate';
+import Home from './views/Home';
 
 function Root() {
   return (
     <Provider>
       <BrowserRouter>
         <Switch>
-          <Route exact path={routes.home} component={MainTemplate} />
+          <Route exact path={routes.home} component={Home} />
           <Route path={routes.favorites} component={MainTemplate} />
         </Switch>
       </BrowserRouter>
--- src/context/index.js
+++ src/context/index.js
@@ -1,13 +1,40 @@
 import React, { createContext, useReducer } from 'react';
-//
-const reducer = '';
-const initial = '';
+import reducer from '../reducer';
 
-//
+const initialState = {
+  favorites: [],
+  fetched: [
+    {
+      id: 'g2E2NQ5SWSU',
+      url:
+        'https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE2MDAzNH0',
+      username: 'abc',
+    },
+    {
+      id: 'g2E2N2Q5SWSU',
+      url:
+        'https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE2MDAzNH0',
+      username: 'abc',
+    },
+    {
+      id: 'g2E2NQ545SWSU',
+      url:
+        'https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE2MDAzNH0',
+      username: 'abc',
+    },
+    {
+      id: 'g2E2NQ5S65757WSU',
+      url:
+        'https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE2MDAzNH0',
+      username: 'abc',
+    },
+  ],
+};
+
 export const AppContext = createContext();
 
 export const Provider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initial);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
